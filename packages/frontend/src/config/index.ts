af7cd5fd78b12e0a237342504d9d1278eb2ca1a6
axense234 <andreicomanescuonline@gmail.com>
2023-03-09 14:06:17 +0200

    Updated production config urls.

--- packages/frontend/src/config/index.ts
+++ packages/frontend/src/config/index.ts
@@ -1,7 +1,13 @@
 const env = process.env.NODE_ENV;
 
-const baseUrl = env === "development" ? "http://localhost:4000" : "";
+const baseUrl =
+  env === "development"
+    ? "http://localhost:4000"
+    : "https://highschool-site-app-server-ca.onrender.com";
 
-const baseSiteUrl = env === "development" ? "http://localhost:3000" : "";
+const baseSiteUrl =
+  env === "development"
+    ? "http://localhost:3000"
+    : "https://highschool-site-app-ca.netlify.app/";
 
 export { baseUrl, baseSiteUrl };
