883380af3afcaa886e3f3b5ec1ce8a487fd44da0
axense234 <andreicomanescuonline@gmail.com>
2023-07-22 14:19:20 +0300

    Fixed type errors.

--- packages/frontend/src/core/interfaces/template/TemplateUpdateStudent.d.ts
+++ packages/frontend/src/core/interfaces/template/TemplateUpdateStudent.d.ts
@@ -2,7 +2,6 @@ import TemplateStudent from "./TemplateStudent";
 
 interface TemplateUpdateStudent extends Partial<TemplateStudent> {
   passwordVer: string;
-  subscription?: string;
 }
 
 export default TemplateUpdateStudent;
--- packages/frontend/src/core/interfaces/template/TemplateUpdateTeacher.d.ts
+++ packages/frontend/src/core/interfaces/template/TemplateUpdateTeacher.d.ts
@@ -2,7 +2,6 @@ import TemplateTeacher from "./TemplateTeacher";
 
 interface TemplateUpdateTeacher extends Partial<TemplateTeacher> {
   passwordVer: string;
-  subscription?: string;
 }
 
 export default TemplateUpdateTeacher;
